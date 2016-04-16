220dc4c622c4ccad2b19f153cd10a053f5e0ce24
Thibault Kummer <bob@coldsource.net>
2016-04-16 23:52:04 +0200

    Initial import

---
+++ notifications/email-php/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+zip evqueue-plugin-notifications-email-php.zip email.php manifest.xml notification-parameters.php notification-parameters.xsl plugin-configuration.php plugin-configuration.xsl
