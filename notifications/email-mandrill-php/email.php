44777649aa03fc5946b59efea853fbee8c325858
Thibault Kummer <bob@coldsource.net>
2023-03-01 15:39:59 +0100

    Added #OUTPUT_...# variable

--- notifications/email-mandrill-php/email.php
+++ notifications/email-mandrill-php/email.php
@@ -18,32 +18,76 @@ if($config_str==false)
 }
 
 // Decode configuration
-$config = json_decode($config_str);
+$config = json_decode($config_str, true);
+
 if($config===null)
 {
 	error_log("Unable to decode json data, configuration is probably not set correctly\n");
 	die(2);
 }
 
-if(!isset($config->notificationconf->subject) || !isset($config->notificationconf->when) || !isset($config->notificationconf->to) || !isset($config->notificationconf->body))
+if(!isset($config['notificationconf']['subject']) || !isset($config['notificationconf']['when']) || !isset($config['notificationconf']['to']) || !isset($config['notificationconf']['body']))
 {
 	error_log("Invalid configuration\n");
 	die(3);
 }
 
+$xmldoc = new DOMDocument();
+$xmldoc->loadXML((string)$config['instance']);
+
+$xpath = new DOMXpath($xmldoc);
+
+// Compute variables for job's output
+// Compute variables for job's tasks output
+$nodes = $xpath->evaluate('//subjobs/job/tasks/task');
+foreach($nodes as $node)
+{
+	$job_name = $node->parentNode->parentNode->getAttribute('name');
+	$task_name = $node->hasAttribute("path")?$node->getAttribute("path"):$node->getAttribute("name");
+	$retval = $node->hasAttribute('retval')?$node->getAttribute('retval'):0;
+	
+	$output = [
+		'name' => $task_name,
+		'output' => $xpath->evaluate("string(output)", $node),
+		'stderr' => $xpath->evaluate("string(stderr)", $node),
+		'log' => $xpath->evaluate("string(log)", $node)
+	];
+
+	// For all tasks
+	if(!isset($vars["#OUTPUT_{$job_name}#"]))
+		$vars["#OUTPUT_{$job_name}#"] = [];
+	
+	$vars["#OUTPUT_{$job_name}#"][] = $output;
+	
+	// For errors
+	if($retval!=0)
+	{
+		if(!isset($vars['#ERRORS#']))
+			$vars['#ERRORS#'] = [];
+		
+		$vars['#ERRORS#'][] = $output;
+	}
+}
+
+
 // Read workflow instance informations from evQueue engine
-$vars = array('#ID#'=>$argv[1]);
-$xml = simplexml_load_string($config->instance);
-$workflow_attributes = $xml->attributes();
-$vars['#NAME#'] = (string)$workflow_attributes['name'];
-$vars['#START_TIME#'] = (string)$workflow_attributes['start_time'];
-$vars['#END_TIME#'] = (string)$workflow_attributes['end_time'];
+$vars['#ID#'] = $argv[1];
+
+$worflow = ($xmldoc->getElementsByTagName('workflow'))->item(0);
+
+$vars['#NAME#'] = (string)$worflow->getAttribute("name");
+$vars['#START_TIME#'] = (string)$worflow->getAttribute("start_time");
+$vars['#END_TIME#'] = (string)$worflow->getAttribute("end_time");
+
+$parameters = $xpath->evaluate('//parameters/parameter');
+foreach($parameters as $parameter)
+	$vars['#PARAMETERS#'][$parameter->getAttribute("name")] = $parameter->textContent;
 
 // Extract mail informations from config
-$to = $config->notificationconf->to;
-$subject = $config->notificationconf->subject;
-$body = $config->notificationconf->body;
-$when = $config->notificationconf->when;
+$to = $config['notificationconf']['to'];
+$subject = $config['notificationconf']['subject'];
+$body = $config['notificationconf']['body'];
+$when = $config['notificationconf']['when'];
 
 if($when!='ON_SUCCESS' && $when!='ON_ERROR' && $when!='ON_BOTH')
 {
@@ -59,27 +103,85 @@ if($when=='ON_ERROR' && $argv[2]==0)
 	die();
 
 // Do variables substitution
-$values = array_values($vars);
-$vars = array_keys($vars);
+foreach($vars as $var => $value)
+{
+	if(!is_array($value))
+	{
+		$subject = str_replace($var,$value,$subject);
+		$body = str_replace($var,$value,$body);
+	}
+}
 
-$subject = str_replace($vars,$values,$subject);
-$body = str_replace($vars,$values,$body);
+// Format variables for HTML
+foreach($vars as $var_name => $tasks)
+{
+	if(substr($var_name, 0, 8)!='#OUTPUT_')
+		continue;
+	
+	$output_html = "";
+	foreach($tasks as $task)
+		$output_html .= "<div><b>{$task['name']}</b><pre>{$task['output']}</pre></div>";
+	
+	$body = str_replace($var_name, $output_html, $body);
+}
+
+if(!empty($vars['#ERRORS#']))
+{
+	$errors_html = '';
+	
+	foreach($vars['#ERRORS#'] as $task)
+	{
+		$errors_html .= "<table cellpadding='10' cellspacing='1' border='1' align='center' style='width:80%'><thead><tr><th>{$task['name']}</th><th>Description</th></tr></thead><tbody>";
+		if(!empty($task['stderr']))
+			$errors_html .= "<tr><td>stderr</td><td><pre style='white-space: pre-line'>{$task['stderr']}<pre></td></tr>";
+		if(!empty($task['log']))
+			$errors_html .= "<tr><td>stderr</td><td><pre style='white-space: pre-line'>{$task['log']}<pre></td></tr>";
+		$errors_html .= '</tbody></table><table><tr><td></td></tr></table>';
+	}
+
+	$body = str_replace('#ERRORS#', $errors_html, $body);
+}
+
+if(!empty($vars['#PARAMETERS#']))
+{
+	$param_html = '<table  cellpadding="10" cellspacing="1" border="1" align="center"  style="width: 80%"><thead><tr><th scope="col">Key</th><th scope="col">Value</th></tr></thead><tbody>';
+	
+	foreach($vars['#PARAMETERS#'] as $name => $param)
+	{
+		if(@unserialize($param) !== false)
+			$param =  json_encode(@unserialize($param));
+
+		$param_html .= "<tr><td>{$name}</td><td><pre style='white-space: pre-line'>{$param}<pre></td></tr>";
+	}
+
+	$param_html .= '</tbody></table><table><tr><td></td></tr></table>';
+
+	$body = str_replace('#PARAMETERS#', $param_html, $body);
+}
 
 // Send email
 $msg = [
-	"key" => $config->pluginconf->key,
+	"key" => $config['pluginconf']['key'],
 	"message" => [
-		"text" => $body,
+		"html" => $body,//$body,
 		"subject" => $subject,
-		"from_email" => $config->pluginconf->from,
-		"from_name" => $config->pluginconf->from_name,
+		"from_email" => $config['pluginconf']['from'],
+		"from_name" => $config['pluginconf']['from_name'],
 		"to" => [["email" => $to,"type" => "to"]]
 	]
 ];
 
+
 $ch = curl_init("https://mandrillapp.com/api/1.0/messages/send.json");
 curl_setopt($ch,CURLOPT_POSTFIELDS,json_encode($msg));
-curl_exec($ch);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+$output = curl_exec($ch);
+
+$curl_infos = curl_getinfo($ch);
+if($curl_infos['http_code']!='200')
+{
+	error_log("Error calling Mandrill : $output\n");
+	die(7);
+}
 
 die();
-?>
